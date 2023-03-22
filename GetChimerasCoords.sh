8f6443ed0792b2b6ca5fa1ddba0399532189bd64
mikaOlami <73030217+mikaOlami@users.noreply.github.com>
2023-03-22 10:51:09 +0200

    Update GetChimerasCoords.sh

--- GetChimerasCoords.sh
+++ GetChimerasCoords.sh
@@ -16,7 +16,7 @@ display_usage() {
 	fi 
 
 chimeras_file=$1;  # chimeras file
-if [ -z "$2" ]; then annots_bed="/home/ls/mikao/LD/Annot_Ld1S/LD_annots.copies.bed"; else annots_bed=$(realpath $2); fi;  # optional annotations bedfile
+if [ -z "$2" ]; then annots_bed="~/LD/Annot_Ld1S/LD_annots.copies.bed"; else annots_bed=$(realpath $2); fi;  # optional annotations bedfile
 
 relative_coords_output=$(basename ${chimeras_file%.*})_relative_coords.out;  # relative coords output file
 
